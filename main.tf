cf86e249ee4ef87451f19bde8de456f9566eaeca
ardhmd <ard.datascientest@gmail.com>
2023-10-10 09:20:16 +0000

    Format files

--- main.tf
+++ main.tf
@@ -9,15 +9,14 @@ module "prometheus_module" {
 module "alb_module" {
   source = "./modules/alb-module"
 
-  vpc_name                  = var.vpc_name
-  az_a_subnet_name          = var.az_a_subnet_name
-  az_b_subnet_name          = var.az_b_subnet_name
-  alb_grafana_release_name  = var.alb_grafana_release_name
-  alb_grafana_chart_path    = var.alb_grafana_chart_path
-  alb_grafana_chart_version = var.alb_grafana_chart_version
-  alb_grafana_values_file   = var.alb_grafana_values_file
-  alb_grafana_alb_name      = var.alb_grafana_alb_name
-  # alb_grafana_cleaned_domain_name = var.alb_grafana_cleaned_domain_name
+  vpc_name                   = var.vpc_name
+  az_a_subnet_name           = var.az_a_subnet_name
+  az_b_subnet_name           = var.az_b_subnet_name
+  alb_grafana_release_name   = var.alb_grafana_release_name
+  alb_grafana_chart_path     = var.alb_grafana_chart_path
+  alb_grafana_chart_version  = var.alb_grafana_chart_version
+  alb_grafana_values_file    = var.alb_grafana_values_file
+  alb_grafana_alb_name       = var.alb_grafana_alb_name
   alb_grafana_fqdn           = "${var.alb_grafana_record_name}.${var.alb_grafana_cleaned_domain_name}"
   alb_grafana_certificateArn = var.alb_grafana_certificateArn
 
